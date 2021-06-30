055f93674132b9308a4ab49fd0f58e31c483396f
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 18:51:29 -0400

    Implementado el componente HeroesCard

--- src/components/heroes/HeroesList.jsx
+++ src/components/heroes/HeroesList.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroesList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className="row row-cols-1 row-cols-md-2 g-4">
       {heroes.map((hero) => {
-        return <li key={hero.id}>{hero.superhero}</li>;
+        return <HeroCard key={hero.id} {...hero} />;
       })}
-    </ul>
+    </div>
   );
 };
