150f95a72b51821f6124a8e8924a1d8494350ea1
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 16:36:35 -0400

    Commit inicial

---
+++ src/HeoresApp.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./HeroesApp.css";
+
+export const HeroesApp = () => {
+  return (
+    <>
+      <h1>Heroes App</h1>
+    </>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { HeroesApp } from "./HeoresApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
