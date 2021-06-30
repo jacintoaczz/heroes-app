f1b5bff9b2f08cfd02fa044a9c34f21069f6f104
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 18:29:46 -0400

    Creado handler para simular el login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-export const LoginPage = () => {
-  return <div></div>;
+export const LoginPage = ({ history }) => {
+  const handleLogin = () => {
+    history.push("/");
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <hr className="divider" />
+
+      <button
+        className="btn btn-outline-success"
+        onClick={() => {
+          handleLogin;
+        }}
+      >
+        Login
+      </button>
+    </>
+  );
 };
