caa148b3a50933d1f7430ecfda9bb168566b6379
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 18:26:22 -0400

    Implementada la lista de heroes

--- src/pages/DCPage.jsx
+++ src/pages/DCPage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { HeroesList } from "../components/heroes/HeroesList";
 
 export const DCPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>DC Page</h1>
+      <hr className="divider" />
+      <HeroesList publisher="DC Comics" />
+    </>
+  );
 };
--- src/pages/MarvelPage.jsx
+++ src/pages/MarvelPage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { HeroesList } from "../components/heroes/HeroesList";
 
 export const MarvelPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>Marvel Page</h1>
+      <hr className="divider" />
+
+      <HeroesList publisher="Marvel Comics" />
+    </>
+  );
 };
