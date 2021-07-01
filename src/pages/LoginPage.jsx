8622731865b1e102cbef62f673e45ddee4d9b99d
Jacinto Acosta <jacintoaczz@gmail.com>
2021-07-01 10:27:09 -0400

    Leyendo y usando el context

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../auth/AuthContext";
+import { types } from "../types/types";
+
+const user = {
+  id: 0,
+  name: "Jacinto Acosta",
+};
 
 export const LoginPage = ({ history }) => {
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = () => {
-    history.push("/");
+    const loginAction = {
+      type: types.authLogin,
+      payload: user,
+    };
+    dispatch(loginAction);
+    history.replace("/");
   };
 
   return (
-    <>
+    <main className="container">
       <h1>Login</h1>
       <hr className="divider" />
 
-      <button
-        className="btn btn-outline-success"
-        onClick={() => {
-          handleLogin;
-        }}
-      >
+      <button className="btn btn-outline-success" onClick={handleLogin}>
         Login
       </button>
-    </>
+    </main>
   );
 };
